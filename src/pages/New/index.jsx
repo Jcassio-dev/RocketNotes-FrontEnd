c7c366f15878e8085ec80e072f091f55fa9b5984
José Cássio <josecassio2013@gmail.com>
2023-03-05 15:39:24 -0300

    add: remove links

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -19,6 +19,10 @@ export function New() {
         setLinks(prevState => [...prevState, newLink]);
         setNewLink("")
     }
+
+    function handleRemoveLink(deleted){
+        setLinks(prevState => prevState.filter(link => link !== deleted))
+    }
     return(
         <Container>
             <Header/>
@@ -45,7 +49,7 @@ export function New() {
                             <NoteItem 
                             key={String(index)}
                             value={link}
-                            onClick={() => { }}
+                            onClick={() => handleRemoveLink(link)}
                             />
                         ))
                         }
